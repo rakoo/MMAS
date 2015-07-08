00a7817d3b94fc5b100a76c0feda593cc517fc41
Matthieu Rakotojaona <matthieu.rakotojaona@gmail.com>
2015-07-08 02:02:01 +0200

    Move parseResponse in its own method

--- main.go
+++ main.go
@@ -34,78 +34,12 @@ type bodyHandler struct {
 
 func (bh *bodyHandler) handler() func(body []byte, ctx *goproxy.ProxyCtx) []byte {
 	return func(body []byte, ctx *goproxy.ProxyCtx) []byte {
-		start := time.Now()
 
-		rs := rollsum.New()
-		rd := bytes.NewReader(body)
-		buf := make([]byte, 0)
-
-		tx, err := bh.db.Begin()
+		changedPreums, err := bh.parseResponse(body)
 		if err != nil {
 			log.Println(err)
 			return body
 		}
-
-		stmt, err := tx.Prepare(sqlUpSert)
-		if err != nil {
-			log.Println(err)
-			return body
-		}
-
-		for {
-			b, err := rd.ReadByte()
-			if err != nil {
-				if err == io.EOF {
-					break
-				} else {
-					log.Println(err)
-					return body
-				}
-			}
-			rs.Roll(b)
-			buf = append(buf, b)
-			if rs.OnSplitWithBits(5) {
-				h := sha1.Sum(buf)
-				_, err := stmt.Exec(buf, h[:], h[:])
-				if err != nil {
-					log.Println(err)
-					return body
-				}
-				buf = buf[:0]
-			}
-		}
-
-		tx.Commit()
-
-		var count, countbytes int
-		err = bh.db.QueryRow(`SELECT COUNT(*), SUM(LENGTH(content)) FROM chunks`).Scan(&count, &countbytes)
-		if err != nil {
-			log.Println(err)
-			return body
-		}
-
-		var dups, dupsbytes int
-		err = bh.db.QueryRow(`SELECT COUNT(*), SUM(LENGTH(content)) FROM chunks WHERE count > 1`).Scan(&dups, &dupsbytes)
-		if err != nil {
-			log.Println(err)
-			return body
-		}
-
-		var preumsCandidate []byte
-		err = bh.db.QueryRow(`SELECT hash FROM chunks ORDER BY count, hash DESC LIMIT 1`).Scan(&preumsCandidate)
-		if err != nil {
-			log.Println(err)
-			return body
-		}
-
-		var changedPreums bool
-		if bytes.Compare(bh.preums, preumsCandidate) != 0 {
-			bh.preums = preumsCandidate
-			changedPreums = true
-		}
-
-		log.Printf("%d dups / %d chunks (%d / %d bytes) \n", dups, count, dupsbytes, countbytes)
-		log.Printf("Parsed response in %v ms\n", time.Since(start).Seconds()*1000)
 
 		if changedPreums {
 			bh.makeDict()
@@ -113,6 +47,75 @@ func (bh *bodyHandler) handler() func(body []byte, ctx *goproxy.ProxyCtx) []byte
 
 		return body
 	}
+}
+
+func (bh *bodyHandler) parseResponse(body []byte) (changedPreums bool, err error) {
+	startParse := time.Now()
+
+	rs := rollsum.New()
+	rd := bytes.NewReader(body)
+	buf := make([]byte, 0)
+
+	tx, err := bh.db.Begin()
+	if err != nil {
+		return false, err
+	}
+
+	stmt, err := tx.Prepare(sqlUpSert)
+	if err != nil {
+		return false, err
+	}
+
+	for {
+		b, err := rd.ReadByte()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return false, err
+			}
+		}
+		rs.Roll(b)
+		buf = append(buf, b)
+		if rs.OnSplitWithBits(5) {
+			h := sha1.Sum(buf)
+			_, err := stmt.Exec(buf, h[:], h[:])
+			if err != nil {
+				return false, err
+			}
+			buf = buf[:0]
+		}
+	}
+
+	tx.Commit()
+
+	var count, countbytes int
+	err = bh.db.QueryRow(`SELECT COUNT(*), SUM(LENGTH(content)) FROM chunks`).Scan(&count, &countbytes)
+	if err != nil {
+		return false, err
+	}
+
+	var dups, dupsbytes int
+	err = bh.db.QueryRow(`SELECT COUNT(*), SUM(LENGTH(content)) FROM chunks WHERE count > 1`).Scan(&dups, &dupsbytes)
+	if err != nil {
+		return false, err
+	}
+
+	var preumsCandidate []byte
+	err = bh.db.QueryRow(`SELECT hash FROM chunks ORDER BY count, hash DESC LIMIT 1`).Scan(&preumsCandidate)
+	if err != nil {
+		return false, err
+	}
+
+	if bytes.Compare(bh.preums, preumsCandidate) != 0 {
+		bh.preums = preumsCandidate
+		changedPreums = true
+	}
+
+	log.Printf("%d dups / %d chunks (%d / %d bytes) \n", dups, count, dupsbytes, countbytes)
+	log.Printf("Parsed response in %v ms\n", time.Since(startParse).Seconds()*1000)
+
+	return changedPreums, nil
 }
 
 func (bh *bodyHandler) makeDict() {
